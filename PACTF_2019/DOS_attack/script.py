87b87274f1b56e2a9f008a401b37b52d10635a55
Gigabytes <49597727+Tera-bytes@users.noreply.github.com>
2020-01-11 03:01:03 +0200

    Update script.py

--- PACTF_2019/DOS_attack/script.py
+++ PACTF_2019/DOS_attack/script.py
@@ -1,7 +1,7 @@
 import re
 import time
-#Before writing this script, I initially extracted all the regex from the logs file (but that's pretty useless; so you might not go for it)
-#to a file named regex.txt
+#Before writing this script, I initially extracted all the regex from the logs file to a file named regex.txt (but that's pretty useless; 
+#so you might not go for it)
 
 f=open("regex.txt","r")  #regex.txt contains all the regular expressions
 g=open("dictionary.txt","r")
@@ -17,3 +17,10 @@ for elmt in regex:
         
     a="%.3f"%(time.time()-start) #NB: a is now a string and not a float
     timing.write(a + "\n")
+    
+    #REMARK:
+    #To reduce your execution time (which is way too large), you can use do the following combined (I noticed it lately):
+    #    *use a single for loop,
+    #    *replace the "findall" instruction by "search()", and
+    #    *instead of taking the content of the dictionary in the form of a list, you could take it in the form of a string using
+    #    read() instead of readlines()
