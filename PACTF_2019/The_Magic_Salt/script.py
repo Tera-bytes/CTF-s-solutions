4679f2232bbb66b525229333c3cf3ba5c7fd862c
Gigabytes <49597727+Tera-bytes@users.noreply.github.com>
2020-01-12 07:38:56 +0200

    Update script.py

--- PACTF_2019/The_Magic_Salt/script.py
+++ PACTF_2019/The_Magic_Salt/script.py
@@ -1,1 +1,13 @@
+from hashlib import md5
 
+Hash="832c8f6be1b48ce9abcd6deb4ebcdc31"
+salt="counterspell"
+f=open("path_to_dictionary","r")
+lst=f.readlines()
+for elmt in lst:
+    elmt=elmt[:-1]
+    password=elmt + salt
+    get_hash=md5(password.encode())
+    if get_hash.hexdigest()==Hash:
+       print(elmt)
+       break
